1b3c8b851f0a3623d7fb581431bfc55d4224661c
rishabjain88 <rishabjain8827@gmail.com>
2021-03-19 17:46:57 +0530

    location and camera in mark attendance

--- src/Screens/PublicLogin.js
+++ src/Screens/PublicLogin.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity,TextInput,} from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,TextInput,Image} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
-
+import { AntDesign } from '@expo/vector-icons';
 export default function PublicLogin() {
 
   const navigation = useNavigation();
@@ -16,6 +16,8 @@ export default function PublicLogin() {
         colors={['rgba(122, 51, 255,0.3)', 'transparent']}
         style={styles.container}
       >
+           <View style={{flexDirection:'row' ,alignItems:'center'}}>
+         <View style={{flexDirection:'column',marginRight:10}}>
         <TextInput
         placeholder="Mobile Number" 
         placeholderTextColor='#B3AFB4'
@@ -29,18 +31,21 @@ export default function PublicLogin() {
        style={styles.txt}
        
       />
-       <TouchableOpacity onPress={()=>{navigation.navigate("Public Home Page")}}>
-      <LinearGradient
+      <TouchableOpacity >
+         <Text style={styles.forg}>Dont have an Account ?, Create One..</Text> 
+         </TouchableOpacity>
+      </View>
+      <TouchableOpacity  onPress={()=>{navigation.navigate("Public Home Page")}}>
+    
      
-        // Button Linear Gradient
-        colors={['#7A33FF', '#000', '#192f6a']}
-        style={styles.button}>
+   
            
-        <Text style={styles.text}>Login</Text>
-       
-      </LinearGradient>
-      
-      </TouchableOpacity>
+    <AntDesign name="login" size={45}  color='#7A33FF' />
+
+
+</TouchableOpacity>
+
+      </View>
       <Image style={{width:"100%",height:170}} source={require('../../assets/city2.png')}/>
    </LinearGradient>
   );
@@ -85,12 +90,21 @@ const styles = StyleSheet.create({
     fontSize:18,
      color:"#fff",
         width:200,
-       padding:5,
-       marginBottom:20,
-    
+       padding:2,
+       marginVertical:4,
        borderWidth:1,
        borderRadius:13,
        borderColor:'#7A33FF',
      
   },
+  forg:{
+      
+    height: 30,
+     
+      fontSize:15,
+     color:"#5B44FF",
+       margin:0,
+      textAlign:'center',
+      textDecorationLine:'underline',
+},
 });
